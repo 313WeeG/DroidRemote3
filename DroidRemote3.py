83e97496fa44ff0858015b0a792bcb2cd75a85ae
313WeeG <313str8fromthad@gmail.com>
2026-07-01 22:33:05 -0400

    Initialize DroidRemote3 project structure
    Initial commit of the DroidRemote3 project structure including main application file and various templates, static files, and scripts.

---
+++ DroidRemote3.py
@@ -0,0 +1,29 @@
+
+DroidRemote3/
+│
+├── app.py
+├── templates/
+│   ├── index.html
+│   ├── login.html
+│   ├── ssh.html
+│   ├── file_explorer.html
+│   ├── mic_capture.html
+│   ├── speak.html
+│   ├── qr_code.html
+│   ├── notification.html
+│   └── media_send.html
+├── static/
+│   └── style.css
+├── users.txt
+├── systemd_service.conf
+├── termux_terminal.sh
+├── brute_pin.sh
+├── reverse_shell.sh
+├── file_explorer.sh
+├── mic_capture.sh
+├── speak.sh
+├── qr_code.sh
+├── media_send.sh
+├── send_notification.sh
+├── DroidRemote3.py
+└── install.sh
